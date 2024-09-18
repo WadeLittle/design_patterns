b7469ee8650b9b5edab9b5b54c981274461d1f27
WadeLittle <wdlittle9@gmail.com>
2024-09-18 14:49:21 -0400

    working singleton design pattern with accurate java documentation

--- singleton/Question.java
+++ singleton/Question.java
@@ -3,11 +3,25 @@ package singleton;
 import java.lang.reflect.Array;
 import java.util.*;
 
+/**
+ * This class holds all the contents of a question broken into parts, question,
+ * answers, and where the correct answer is in the list of answers
+ * 
+ * @author Wade Little
+ */
 public class Question {
     private String question;
     private ArrayList<String> answers;
     private int correctAnswer;
 
+    /**
+     * Creates a question based on the given paramaters
+     * 
+     * @param question      The question to be asked as a string
+     * @param correctAnswer The index where the correct answer is found in the array
+     *                      of answers
+     * @param answers       An array of answers
+     */
     public Question(String question, int correctAnswer, String[] answers) {
         this.question = question;
         this.correctAnswer = correctAnswer;
@@ -17,19 +31,36 @@ public class Question {
         }
     }
 
-    public String toString() {       
-        String printString = question + "\n" + "1. " + answers.get(0) + "\n2. " + answers.get(1) + "\n3. " + answers.get(2) + "\n4. " + answers.get(3);
+    /**
+     * Displays the question and its answer choices in an easy to read format
+     * 
+     * @return The string value of all of the questions contents
+     */
+    public String toString() {
+        String printString = question + "\n" + "1. " + answers.get(0) + "\n2. " + answers.get(1) + "\n3. "
+                + answers.get(2) + "\n4. " + answers.get(3);
         return printString;
     }
 
+    /**
+     * Compares the users answer choice to the correct answer choice
+     * 
+     * @param userAnswer The user inputted answer
+     * @return True if the user is correct and false if the user is incorrect
+     */
     public boolean isCorrect(int userAnswer) {
-        if (userAnswer == correctAnswer) {
+        if (userAnswer - 1 == correctAnswer) {
             return true;
         } else {
             return false;
         }
     }
 
+    /**
+     * Gets the correct answer from the arrayList of answers
+     * 
+     * @return The string of the correct answer
+     */
     public String getCorrectAnswer() {
         return answers.get(correctAnswer);
     }
--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -2,6 +2,11 @@ package singleton;
 
 import java.util.*;
 
+/**
+ * Creates the Trivia Game and only allows there to be one created
+ * 
+ * @author Wade Little
+ */
 public class TriviaGame {
 
     private static TriviaGame triviaGame;
@@ -10,6 +15,10 @@ public class TriviaGame {
     private Random rand;
     private ArrayList<Question> questions;
 
+    /**
+     * Private initiliaztion of the Trivia Game and sets the variabes to their
+     * default values.
+     */
     private TriviaGame() {
         this.score = 0;
         this.numGames = 0;
@@ -17,6 +26,12 @@ public class TriviaGame {
         this.rand = new Random();
     }
 
+    /**
+     * Creates an instance of the TriviaGame if one isn't made yet otherwise it
+     * returns the already created trivia game
+     * 
+     * @return The instance of the TriviaGame
+     */
     public static TriviaGame getInstance() {
         if (triviaGame == null) {
             triviaGame = new TriviaGame();
@@ -24,13 +39,24 @@ public class TriviaGame {
         return triviaGame;
     }
 
+    /**
+     * Gets a random question from the arrayList of questions
+     * 
+     * @return A random question
+     */
     public Question getQuestion() {
         if (hasMoreQuestions() == false) {
             return null;
         } else
-            return questions.get(rand.nextInt());
+            return questions.remove(rand.nextInt(questions.size()));
     }
 
+    /**
+     * Lets us know if there is any questions left to be asked
+     * 
+     * @return True if there is more questions and false if there is no more
+     *         questions in the questions arrayList
+     */
     public boolean hasMoreQuestions() {
         if (questions.size() >= 1) {
             return true;
@@ -39,17 +65,28 @@ public class TriviaGame {
         }
     }
 
+    /**
+     * Increses the score and number of games played
+     */
     public void winRound() {
         this.score++;
         this.numGames++;
     }
 
+    /**
+     * Only increases the number of games played
+     */
     public void loseRound() {
         this.numGames++;
     }
 
+    /**
+     * Gets the score (score/numGames) played
+     * 
+     * @return A string with an easy to read format of the score
+     */
     public String getScores() {
-        return String.valueOf(this.score) + "/" + String.valueOf(this.numGames);
+        return "You scored " + String.valueOf(this.score) + "/" + String.valueOf(this.numGames);
     }
 
 }
